67bd7b834f5f59ab0d3c032e94a47d55cd3ec988
rtgiles <31195013+rtgiles@users.noreply.github.com>
2017-10-18 14:15:00 -0500

    most recent update still not working

--- giphy.js
+++ giphy.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	var aninmalArray=["Lions","Leopards"];
+	var animalArray=["Lions","Leopards"];
+	var animalArray2=[];
 	//console.log(aninmalArray);
 	
 
@@ -13,7 +14,7 @@ $(document).ready(function() {
         $("#animalButtons").empty();
 
         // Looping through the array of animals
-        for (var i = 0; i < aninmalArray.length; i++) {
+        for (var i = 0; i < animalArray.length; i++) {
 
           // Then dynamicaly generating buttons for each animal in the array
           // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
@@ -21,15 +22,15 @@ $(document).ready(function() {
           // Adding a class of animal to our button
           a.addClass("animal");
           // Adding a data-attribute
-          a.attr("data-animal", aninmalArray[i]);
+          a.attr("data-animal", animalArray[i]);
           // Providing the initial button text
-          a.text(aninmalArray[i]);
+          a.text(animalArray[i]);
           // Adding the button to the HTML
           $("#animalButtons").append(a);
         }
     }
 
-    //$(document).on("click", ".aninmalArray", animalsName)
+    
     createAnimalsbuttons();
 
 		$("#addAnimal").on("click", function(event) {
@@ -39,7 +40,7 @@ $(document).ready(function() {
 	        var animal = $("#animalInput").val().trim();
 
 	        // Adding input to array
-	        aninmalArray.push(animal);
+	        animalArray.push(animal);
 	        console.log(aninmalArray);
 	        // Calling createButtons which processes the animal array
 	        createAnimalsbuttons();
@@ -47,20 +48,21 @@ $(document).ready(function() {
 	      });
 
 
-		/*$("#animalButtons")*/$(document.body).on("click", ".animal", function(event){
-		var animal2 = $(this).attr("data-animal");
+		$(document.body).on("click", ".animal", function(event){
+			var animal2 = $(this).attr("data-animal");
 
-		console.log(animal2);
-	      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-	        animal2 + "&api_key=dc6zaTOxFJmzC&limit=10";
+			//console.log(animal);
+		    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+		      animal2 + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-	      $.ajax({
-	          url: queryURL,
-	          method: "GET"
-	        })
+		      $.ajax({
+		          url: queryURL,
+		          method: "GET"
+		        })
 		        .done(function(response) {
 		        	var results = response.data;
-
+		        	//console.log(response.data);
+		        	//console.log(results);
 		          for (var i = 0; i < results.length; i++) {
 		            var gifDiv = $("<div class='item'>");
 
@@ -70,13 +72,33 @@ $(document).ready(function() {
 
 		            var animalImage = $("<img>");
 		            animalImage.attr("src", results[i].images.fixed_height.url);
+		            animalImage.attr("data-state", "animate");
+		            animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+		            animalImage.attr("data-animate", results[i].images.fixed_height.url);
+		            animalArray2.push(animalImage);
+		            console.log(animalArray2); 
 
 		            gifDiv.prepend(p);
-		            gifDiv.prepend(animalImage);
+		            gifDiv.prepend(animalArray2[i]);
 
 		            $("#giphyAppearshere").prepend(gifDiv);
-		        	}
+		            }
 		        }) 
-		          
+		    $(document.body).on("click", ".item", function(event){
+		            	var state = $(this).attr("data-state");
+					      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+					      // Then, set the image's data-state to animate
+					      // Else set src to the data-still value
+					      if (state === "animate") {
+					        $(this).attr("src", $(this).attr("data-still"));
+					        $(this).attr("data-state", "still");
+					      } else {
+					        $(this).attr("src", $(this).attr("data-animate"));
+					        $(this).attr("data-state", "animate");
+					      }
+		            	
+		            });      
 	    });
 })
+
+
